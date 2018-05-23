79cddbc19d325865c0641bb018e64089a29022b5
Maxime RECUERDA <maxime.recuerda@outlook.fr>
2018-05-23 12:33:20 +0200

    adding bases of waitQuestion

--- APP4-QCM-Front/src/WaitQuestionFrame.java
+++ APP4-QCM-Front/src/WaitQuestionFrame.java
@@ -2,4 +2,18 @@ import javax.swing.JDialog;
 
 public class WaitQuestionFrame extends JDialog {
 
+	private WaitQuestionFrame(JDialog dialog) {
+		super(dialog, "Wait Question", true);
+		
+		initialize();
+	}
+	
+	void initialize() {
+		
+	}
+	
+	public static void show(JDialog dialog) {
+		WaitQuestionFrame waitQuestion = new WaitQuestionFrame(dialog);
+		waitQuestion.setVisible(true);
+	}
 }
