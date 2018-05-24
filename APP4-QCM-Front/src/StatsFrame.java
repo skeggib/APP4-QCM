2a8d8d9849a4e142cd67335f87fcc6c3f84f9f5a
dimitri mikhailoff <dmikhailof@gmail.com>
2018-05-24 13:27:32 +0200

    Message errors while creating session

---
+++ APP4-QCM-Front/src/StatsFrame.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+import javax.swing.JDialog;
+
+import com.app4qcm.database.Question;
+
+public class StatsFrame extends JDialog {
+	
+	
+	
+	public static void show(JDialog dialog, Question[] questionList) {
+		Question q1 = new Question("Q1 : Quelle est la couleur du cheval blanc d'HenryIV ?", "Rouge", "Bleu", "Noir", "Blanc", false, false, false, false);
+		Question q2 = new Question("Q1 : Quelle est la couleur du cheval blanc d'HenryIV ?", "Rouge", "Bleu", "Noir", "Blanc", false, false, false, false);
+		Question q3 = new Question("Q1 : Quelle est la couleur du cheval blanc d'HenryIV ?", "Rouge", "Bleu", "Noir", "Blanc", false, false, false, false);
+		Question q4 = new Question("Q1 : Quelle est la couleur du cheval blanc d'HenryIV ?", "Rouge", "Bleu", "Noir", "Blanc", false, false, false, false);
+
+		questionList[0] = q1;
+		questionList[1] = q2;
+		questionList[2] = q3;
+		questionList[3] = q4;
+		
+		
+		
+	}
+}
