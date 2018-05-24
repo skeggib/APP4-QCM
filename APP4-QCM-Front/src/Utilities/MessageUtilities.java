a0dc04e9f68df917a927360dfe898c2d8c36c22f
Maxime RECUERDA <maxime.recuerda@outlook.fr>
2018-05-24 13:13:25 +0200

    adding generic errors messages

--- APP4-QCM-Front/src/Utilities/MessageUtilities.java
+++ APP4-QCM-Front/src/Utilities/MessageUtilities.java
@@ -8,4 +8,16 @@ public class MessageUtilities {
 	public static void showError(String message, String title) {
 		new JOptionPane().showMessageDialog(null, message, title, JOptionPane.ERROR_MESSAGE);
 	}
+
+	public static void showError(Class c) {
+		switch (c.getName()) {
+		case "java.net.IOException":
+			showError("Failed to call Server", "Server Call");
+			break;
+		case "java.net.UnknownHostException":
+			default:
+			showError("Unknown error.", "Unknown error");
+			break;
+		}
+	}
 }
